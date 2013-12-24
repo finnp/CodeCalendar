927065a81b0dfdfa520362bbeb25aa1808f2c625
finn <derfinn@gmail.com>
2013-12-24 16:16:55 +0100

    Return in 2014

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ app = Flask(__name__)
 @app.route('/', defaults = {'month': 0, 'day': 0})
 @app.route('/<int:month>/<int:day>')
 def showdate(month, day):
-    if(month == 0 and day == 0):
-        show = datetime.now()
-    else:
-        show = datetime(datetime.now().year, month, day)
-    file_path = show.strftime('html/%m-%d.html')
-    try: 
-        return open(file_path).read()
-    except IOError:
-        return 'Not found', 404
+        if(month == 0 and day == 0):
+            if datetime.now().year < 2014:
+                return 'It is not 2014, yet. Bookmark this and return on the first of January'
+            show = datetime.now()
+        else:
+            show = datetime(datetime.now().year, month, day)
+        file_path = show.strftime('html/%m-%d.html')
+        try: 
+            return open(file_path).read()
+        except IOError:
+            return 'Not found', 404
+    
 
 @app.route('/humans.txt')
 def humans():
